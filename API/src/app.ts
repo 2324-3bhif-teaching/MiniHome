c80181d3cdb7f24416ffd02a03dd5f1af0e33a52
Alexander Resch <alexander@resch-web.at>
2024-03-21 10:02:56 +0100

    projekt struktur created

---
+++ API/src/app.ts
@@ -0,0 +1,10 @@
+﻿import express, {request,response} from 'express';
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}.`);
+});
